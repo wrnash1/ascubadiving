c2c166300dccdac425bcd020ee71876732cc7633
wrnash1 <wrnash1@gmail.com>
2023-05-12 15:46:22 -0500

    bugfix

--- add_diver/models.py
+++ add_diver/models.py
@@ -4,40 +4,63 @@ from phone_field import PhoneField
 
 # Create your models here.
 class add_diver(models.Model):
-    name = models.CharField(max_length=100, help_text='First and Last Name')
+    name = models.CharField(max_length=100, help_text="First and Last Name")
     certification_number = models.IntegerField()
     agency = (
-        ('PADI', 'PADI'),
-        ('NAUI', 'NAUI'),
-        ('SSI', 'SSI'),
-        ('BSAC', 'BSAC'),
-        ('CMAS', 'CMAS'),
-        ('SDI', 'SDI'),
-        ('TDI', 'TDI'),
-        ('IANTD', 'IANTD'),
-        ('SDI', 'SDI'),
+        ("PADI", "PADI"),
+        ("SSI", "SSI"),
+        ("NAUI", "NAUI"),
+        ("SDI", "SDI"),
+        ("CMAS", "CMAS"),
+        ("BSAC", "BSAC"),
+        ("ACUC", "ACUC"),
+        ("YMCA", "YMCA"),
+        ("NASDS", "NASDS"),
+        ("IANTD", "IANTD"),
+        ("PDIC", "PDIC"),
+        ("SEI", "SEI"),
+        ("TDI", "TDI"),
+        ("GUE", "GUE"),
+        ("RAID", "RAID"),
+        ("ANDI", "ANDI"),
+        ("FMAS", "FMAS"),
+        ("FFESSM", "FFESSM"),
+        ("SSAC", "SSAC"),
+        ("SAA", "SAA"),
+        ("CMAS", "CMAS"),
     )
     certification_agency = models.CharField(max_length=100, choices=agency)
+
+    level = (
+        ("Open Water", "Open Water"),
+        ("Advanced Open Water", "Advanced Open Water"),
+        ("Rescue Diver", "Rescue Diver"),
+        ("Divemaster", "Divemaster"),
+        ("Instructor", "Instructor"),
+    )
+    certification_level = models.CharField(max_length=100, choices=level)
+
     Date_of_birth = models.DateField()
     insurance = (
-        ('Yes', 'Yes'),
-        ('No', 'No'),
+        ("Yes", "Yes"),
+        ("No", "No"),
     )
-    student_insurance = models.CharField(max_length=5, choices=insurance, help_text='Yes or No')
-    dan_insurance = models.CharField(max_length=100, blank=True, help_text='Type in Dan Insurance Number')
+    student_insurance = models.CharField(
+        max_length=5, choices=insurance, help_text="Yes or No"
+    )
     email = models.EmailField(max_length=100)
-    phone_number = PhoneField(blank=True, help_text='Contact phone number')
-    address = models.CharField(max_length=100, help_text='Street Address')
-    city = models.CharField(max_length=100, help_text='City')
-    state = models.CharField(max_length=100, help_text='State')
-    zip = models.IntegerField(help_text='Zip Code')
+    phone_number = PhoneField(blank=True, help_text="Contact phone number")
+    address = models.CharField(max_length=100, help_text="Street Address")
+    city = models.CharField(max_length=100, help_text="City")
+    state = models.CharField(max_length=100, help_text="State")
+    zip = models.IntegerField(help_text="Zip Code")
     SHIRT_SIZES = (
-        ('S', 'Small'),
-        ('M', 'Medium'),
-        ('L', 'Large'),
-        ('XL', 'Extra Large'),
-        ('XXL', 'Extra Extra Large'),
-        ('XXXL', 'Extra Extra Extra Large'),
+        ("S", "Small"),
+        ("M", "Medium"),
+        ("L", "Large"),
+        ("XL", "Extra Large"),
+        ("XXL", "Extra Extra Large"),
+        ("XXXL", "Extra Extra Extra Large"),
     )
     shirt_size = models.CharField(max_length=4, choices=SHIRT_SIZES)
 
--- add_diver/views.py
+++ add_diver/views.py
@@ -1,6 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import add_diver
+
+# def add_diver(request):
+#    # Code to handle the admin/add_divers URL goes here
+#    return render(request, "add_diver.html")
 
 
 def add_diver(request):
-    # Code to handle the admin/add_divers URL goes here
+    if request.method == "POST":
+        name = request.POST.get("name")
+        certification_number = request.POST.get("certification_number")
+        certification_level = request.POST.get("certification_level")
+        certification_agency = request.POST.get("certification_agency")
+        date_of_birth = request.POST.get("Date_of_birth")
+        dan_insurance = request.POST.get("Dan_insurance")
+        email = request.POST.get("email")
+        phone = request.POST.get("phone")
+        address = request.POST.get("address")
+        zipcode = request.POST.get("zipcode")
+        shirt_size = request.POST.get("shirt_size")
+
+        # Create a new Diver object with the submitted data
+        diver = add_diver(
+            name=name,
+            certification_number=certification_number,
+            certification_level=certification_level,
+            certification_agency=certification_agency,
+            date_of_birth=date_of_birth,
+            dan_insurance=dan_insurance,
+            email=email,
+            phone=phone,
+            address=address,
+            zipcode=zipcode,
+            shirt_size=shirt_size,
+        )
+
+        # Save the diver object to the database
+        diver.save()
+
+        # Redirect to a success page or do any additional processing
+        return redirect("success_page")
+
+    # Render the form template if it's a GET request
     return render(request, "add_diver.html")
