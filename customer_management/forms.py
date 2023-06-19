8e4781cffb19f8e3609208413ee48497488cee07
Bill Nash <wrnash1@gmail.com>
2023-06-18 21:42:00 -0500

    Delete customer_management directory

--- customer_management/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from diveshopmanagement.models import Customer
-
-
-class CustomerForm(forms.ModelForm):
-    class Meta:
-        model == Customer
-        fields == "__all__"
