164bb2d09e2c2837bde179dd19c62bed1ecd7724
Bill Nash <wrnash1@gmail.com>
2023-06-18 21:41:48 -0500

    Delete compressor directory

--- compressor/admin.py.bak
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from diveshopmanagement.models import Compressor
-
-admin.site.register(Compressor)
--- compressor/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from diveshopmanagement.models import Compressor
-
-
-class CompressorForm(forms.ModelForm):
-    class Meta:
-        model == Compressor
-        fields == "__all__"  # Or specify the fields you want to include in the form
