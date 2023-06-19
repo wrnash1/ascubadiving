7fd1ebd86bd2a726d900e9c964fed7c6dd3f30ef
Bill Nash <wrnash1@gmail.com>
2023-06-18 21:43:13 -0500

    Delete static directory

--- static/js/base.js
+++
@@ -1,8 +0,0 @@
-// Custom JavaScript for the base.js template
-
-// Update current date and time
-setInterval(function () {
-    var now = new Date();
-    var dateTimeString = now.toLocaleString();
-    document.getElementById('currentDateTime').textContent = dateTimeString;
-}, 1000);
--- static/js/homepage.js
+++
@@ -1,5 +0,0 @@
-// Toggle navbar collapse on menu icon click
-//hoempage.js
-document.querySelector('.menu-icon').addEventListener('click', function () {
-    document.querySelector('.navbar-collapse').classList.toggle('show');
-});
